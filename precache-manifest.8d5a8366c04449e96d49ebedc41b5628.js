e3dd3dd5d81df4aca9db9278ed1464078b2a85b3
Yakau Trafimovich <5243212@gmail.com>
2021-12-22 09:43:10 +0300

    Updates

---
+++ precache-manifest.8d5a8366c04449e96d49ebedc41b5628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "558c00708fea308988c4",
+    "url": "/got_db/static/css/main.6b61942f.chunk.css"
+  },
+  {
+    "revision": "558c00708fea308988c4",
+    "url": "/got_db/static/js/main.558c0070.chunk.js"
+  },
+  {
+    "revision": "fdeb9f8f9ac622ae75b7",
+    "url": "/got_db/static/css/1.e9b77e15.chunk.css"
+  },
+  {
+    "revision": "fdeb9f8f9ac622ae75b7",
+    "url": "/got_db/static/js/1.fdeb9f8f.chunk.js"
+  },
+  {
+    "revision": "d921aee02c889999424d",
+    "url": "/got_db/static/js/runtime~main.d921aee0.js"
+  },
+  {
+    "revision": "6fdafc7f8007a5cda4c209251d9a6cf3",
+    "url": "/got_db/static/media/error.6fdafc7f.gif"
+  },
+  {
+    "revision": "86f968f5fb7a6b23baca133332d4402b",
+    "url": "/got_db/index.html"
+  }
+];
